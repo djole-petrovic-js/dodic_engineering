797bd11f4960cd015633ad732b0bd30d98e87353
Djordje Petrovic <djordjepetrovic@Djordjes-MacBook-Pro.local>
2021-11-28 12:44:05 +0100

    Added site content

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts');
+var i18n = require('i18n');
+
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var contactRouter = require('./routes/contact');
+var companyRouter = require('./routes/company');
+var servicesRouter = require('./routes/services');
 
 var app = express();
 
@@ -13,14 +18,49 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(expressLayouts);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+i18n.configure({
+  locales: ['en', 'de', 'sr'],
+  defaultLocale: 'en',
+  cookie: 'locale',
+  directory: path.join(__dirname, 'locales')
+});
+
+app.use(i18n.init);
+
+app.use(function (req, res, next) {
+  var url = req.url;
+
+  var mainBgImages = {
+    '/':'',
+    '/company':'tm-about-bg',
+    '/services':'tm-services-bg',
+    '/contact':'tm-contact-bg',
+  };
+
+  res.locals = {
+    chosenUrl: url,
+    mainBgImage: mainBgImages[url],
+    __: i18n.__,
+    // siteTitle: "My Website's Title",
+    // pageTitle: "The Home Page",
+    // author: "Cory Gross",
+    // description: "My app's description",
+  };
+
+  next();
+});
+
+app.use('/contact', contactRouter);
+app.use('/services', servicesRouter);
+app.use('/company', companyRouter);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/company.js
+++ routes/company.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('index');
+router.get('/', function(req, res, next) {
+  res.render('company');
 });
 
 module.exports = router;
